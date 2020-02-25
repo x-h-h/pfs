4d4804a803cee6d6d96a31a476a53cc061c2d429
FluentB <236718094@qq.com>
2020-02-26 00:24:10 +0800

    alloc test

--- super.c
+++ super.c
@@ -291,9 +291,16 @@ static struct file_system_type pfs_fs_type = {
 
 static int __init init_pfs_fs(void)
 {
-	table *t;
-    initHashTable(t);
-    printk("%d\n",t->bucket[1].key);
+	//table *t;
+    //initHashTable(t);
+    struct page *page;
+	void *address;
+	page = alloc_pages(GFP_KERNEL, 0);
+	char data[] = "hello linux";
+	address = page_address(page);
+	memcpy(address, data, strlen(data));
+	printk(KERN_ALERT "%s\n", (char *)address);
+    //printk("%d\n",t->bucket[1].key);
 	int	err;
 
 	if((err = init_inodecache()))
