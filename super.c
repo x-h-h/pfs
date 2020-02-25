4920d914694c36efc58e474213a8a71187730d74
FluentB <236718094@qq.com>
2020-02-26 01:01:39 +0800

    t test debug

--- super.c
+++ super.c
@@ -299,7 +299,7 @@ static int __init init_pfs_fs(void)
 	table *t;
 	//int data[] = {1,2,3};
 	//char data[] = "hello linux";
-	for (i = 0; i < 10; ++i) {
+	for (int i = 0; i < 10; ++i) {
         t->bucket[i].key = NULL;
     }
 	address = page_address(page);
