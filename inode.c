b8bdcdc853a12bbe47714cea28fae71a26e79a49
FluentB <236718094@qq.com>
2020-02-26 18:51:02 +0800

    test

--- inode.c
+++ inode.c
@@ -304,11 +304,11 @@ static int pfs_get_block(struct inode *inode, sector_t block, struct buffer_head
 	struct pfs_sb_info *sbi = PFS_SB(sb);
 	printk("%d\n",sbi->s_spb->s_mark);
 	void * address;
-    //table t[1024];
+    table t[1024];
     if(sbi->s_spb->s_mark == 0)
     {
-    	//initHashTable(t,1024,address);
-    	sbi->s_spb->s_mark == 1;
+    	initHashTable(t,1024,address);
+    	sbi->s_spb->s_mark = 1;
     }
 
 	printk(KERN_ALERT "%p\n", address);
