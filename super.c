a8b06bd5400de9f6195c90fc41256bdd5c538faf
FluentB <236718094@qq.com>
2020-02-27 01:17:10 +0800

    test

--- super.c
+++ super.c
@@ -233,22 +233,11 @@ static int __init init_pfs_fs(void)
     char data[] = "hello linux";
 
     page = alloc_pages(GFP_KERNEL, 0);
-    if (!page)
-    {
-                err("alloc_pages");
-                return -1;
-        }
+    address = page_address(page);
 
-        address = page_address(page);
-        if (!address)
-        {
-                err("page_address");
-                goto err;
-        }
-
-        memcpy(address, data, strlen(data));
-        printk(KERN_ALERT "%s\n", (char *)address);
-        free_pages((unsigned long)address, 0);
+    memcpy(address, data, strlen(data));
+    printk(KERN_ALERT "%s\n", (char *)address);
+    free_pages((unsigned long)address, 0);
 
 	if((err = init_inodecache()))
 		return err;
